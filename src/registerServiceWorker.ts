cc0ae096f1692dd6ba177db642a6dd15d180e592
Terry Qi <fortianrui4@gmail.com>
2020-08-06 21:52:20 +1200

    Fix

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -3,7 +3,6 @@
 import { register } from 'register-service-worker'
 
 export const swUpdated = 'swUpdated';
-
 if ( process.env.NODE_ENV === 'production' ) {
     register( `${process.env.BASE_URL}service-worker.js`, {
         ready() {
