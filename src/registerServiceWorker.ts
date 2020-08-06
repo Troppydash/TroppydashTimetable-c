4dc2e840d0ce1887061f94bf62836a9b96b9883f
Terry Qi <fortianrui4@gmail.com>
2020-08-06 21:54:40 +1200

    Fix

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -3,32 +3,32 @@
 import { register } from 'register-service-worker'
 
 export const swUpdated = 'swUpdated';
-if ( process.env.NODE_ENV === 'production' ) {
-    register( `${process.env.BASE_URL}service-worker.js`, {
-        ready() {
-            console.log(
-                'App is being served from cache by a service worker.'
-            )
-        },
-        updated( registration: ServiceWorkerRegistration ) {
-            document.dispatchEvent(
-                new CustomEvent( swUpdated, { detail: registration } )
-            );
-        },
-        registered() {
-            //
-        },
-        cached() {
-            //
-        },
-        updatefound() {
-            //
-        },
-        offline() {
-            //
-        },
-        error( error ) {
-            //
-        }
-    } )
-}
+// if ( process.env.NODE_ENV === 'production' ) {
+register( `${process.env.BASE_URL}service-worker.js`, {
+    ready() {
+        console.log(
+            'App is being served from cache by a service worker.'
+        )
+    },
+    updated( registration: ServiceWorkerRegistration ) {
+        document.dispatchEvent(
+            new CustomEvent( swUpdated, { detail: registration } )
+        );
+    },
+    registered() {
+        //
+    },
+    cached() {
+        //
+    },
+    updatefound() {
+        //
+    },
+    offline() {
+        //
+    },
+    error( error ) {
+        //
+    }
+} )
+// }
