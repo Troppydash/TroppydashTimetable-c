660da1cef8e7043f040d0265aed149b00a3f708b
Terry Qi <fortianrui4@gmail.com>
2020-09-23 21:26:36 +1200

    Fix

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,4 @@
 module.exports = {
-    configureWebpack:{
-        optimization: {
-            splitChunks: {
-                minSize: 10000,
-                maxSize: 250000,
-            }
-        }
-    },
     pwa: {
         name: "STimetable (Dev)",
         themeColor: "#b82832"
